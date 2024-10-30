9087b8baa593dc104939616cc1cf7a6540314100
elvischepin-github <hello.elvis@icloud.com>
2024-10-31 00:17:24 +0200

    v1.0 Priespaskutine Sinchronizacija

--- SP-1-studentu-duomenys/main.cpp
+++ SP-1-studentu-duomenys/main.cpp
@@ -1,124 +1,13 @@
-#include "libraries.h"
-#include "studentas.h"
-
-//START Atminties Matavimo Funkcija// Windows Windows Windows
-//void rodytiAtmintiesSanaudasWindows() {
-//    PROCESS_MEMORY_COUNTERS pmc;
-//    if (GetProcessMemoryInfo(GetCurrentProcess(), &pmc, sizeof(pmc))) {
-//        std::cout << "Atminties sąnaudos po rūšiavimo funkcijos: " << pmc.WorkingSetSize / 1024 << " KB" << std::endl;
-//    } else {
-//        std::cerr << "Negalima pasiekti atminties sąnaudų." << std::endl;
-//    }
-//} //END Atminties Matavimo Funkcija//
-
-//START Atminties Matavimo Funkcija// macOS macOS macOS
-void rodytiAtmintiesSanaudasMacOS() {
-    struct rusage usage;
-    if (getrusage(RUSAGE_SELF, &usage) == 0) {
-        std::cout << "Atminties sąnaudos po rūšiavimo funkcijos: " << usage.ru_maxrss << " KB" << std::endl;
-    } else {
-        std::cerr << "Negalima pasiekti atminties sąnaudų." << std::endl;
-    }
-} //END Atminties Matavimo Funkcija//
-
-//START Rusiavimo Dviems Failams Funkcija//
-void rusiuotiStudentusDviejoseFailuose(const std::string& sablonuFailas, const std::string& geriFailas, const std::string& blogiFailas){
-    
-    std::ifstream skaitomasis(sablonuFailas);
-    std::ofstream geriIrasymas(geriFailas);
-    std::ofstream blogiIrasymas(blogiFailas);
-    
-    std::string eile;
-    
-    if(skaitomasis.is_open() && geriIrasymas.is_open() && blogiIrasymas.is_open()){
-        std::getline(skaitomasis, eile);
-        geriIrasymas << eile << "\n";
-        blogiIrasymas << eile << "\n";
-        
-        while(std::getline(skaitomasis, eile)){
-            std::stringstream ss(eile);
-            std::string vardas, pavarde;
-            std::vector<int> pazymiai(5);
-            int egzaminas;
-            float vidurkis;
-            
-            ss >> vardas >> pavarde;
-            for (int i = 0; i < 5; i++){
-                ss >> pazymiai[i];
-            }
-            ss >> egzaminas;
-            ss >> vidurkis;
-            
-            if(vidurkis >= 5.0){
-                geriIrasymas << eile << "\n";
-            } else blogiIrasymas << eile << "\n";
-        }
-        std::cout << "Surūšiuota √" << std::endl;
-    } else {
-        std::cerr << "Klaida. Rūšiavimas nepavyko, failai nebuvo atidaryti!";
-    }
-    
-    rodytiAtmintiesSanaudasMacOS();
-} //END Rusiavimo Dviems Failams Funkcija//
-
-//START Rusiavimo Vienam Failui Funkcija//
-void rusiuotiStudentusVienameFaile(const std::string& sablonuFailas, const std::string& blogiFailas) {
-    std::ifstream skaitomasis(sablonuFailas);
-    std::ofstream blogiIrasymas(blogiFailas);
-    
-    std::string eile;
-//    std::vector<std::string> likusiosEilutes; // VECTOR
-//    std::list<std::string> likusiosEilutes; // LIST
-    std::deque<std::string> likusiosEilutes; // DEQUE
-    
-    if (skaitomasis.is_open() && blogiIrasymas.is_open()) {
-        std::getline(skaitomasis, eile);
-        blogiIrasymas << eile << "\n";
-        likusiosEilutes.push_back(eile);
-        
-        while (std::getline(skaitomasis, eile)) {
-            std::stringstream ss(eile);
-            std::string vardas, pavarde;
-            std::vector<int> pazymiai(5);
-            int egzaminas;
-            float vidurkis;
-            
-            ss >> vardas >> pavarde;
-            for (int i = 0; i < 5; i++) {
-                ss >> pazymiai[i];
-            }
-            ss >> egzaminas;
-            ss >> vidurkis;
-            
-            if (vidurkis < 5.0) {
-                blogiIrasymas << eile << "\n";
-            } else {
-                likusiosEilutes.push_back(eile);
-            }
-        }
-
-        skaitomasis.close();
-        blogiIrasymas.close();
-        
-        std::ofstream atnaujintasFailas(sablonuFailas, std::ios::trunc);
-        for (const auto& line : likusiosEilutes) {
-            atnaujintasFailas << line << "\n";
-        }
-        
-        std::cout << "Surūšiuota ir pašalinta iš originalaus failo √" << std::endl;
-    } else {
-        std::cerr << "Klaida. Rūšiavimas nepavyko, failai nebuvo atidaryti!";
-    }
-
-    rodytiAtmintiesSanaudasMacOS();
-} //END Rusiavimo Vienam Failui Funkcija//
+#include "visi.h/libraries.h"
+#include "visi.h/studentas.h"
+#include "visi.h/funkcijos.h"
 
 int main() {
     srand((unsigned int)time(0));
     
-//    std::vector<Studentas> studentai; // VECTOR
+    std::vector<Studentas> studentai; // VECTOR
 //    std::list<Studentas> studentai; // LIST
-    std::deque<Studentas> studentai; // DEQUE
+//    std::deque<Studentas> studentai; // DEQUE
     
     std::cout << "Pasirinkite sąrašo dydį įvesdami atitinkamą skaičių:" << std::endl;
     std::cout << "1 - 1 000" << std::endl;
